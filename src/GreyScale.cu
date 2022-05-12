e9778e2916f8bdb601d9639690a865203c59dd41
Jordan Mata <jordanmata15@gmail.com>
2022-05-11 20:02:27 -0700

    Added cross platform file separation handling.

--- src/GreyScale.cu
+++ src/GreyScale.cu
@@ -3,41 +3,35 @@
 #include <cstring>
 
 #include <cstdio>
-#include <jpeglib.h>
-#include <jerror.h>
 
 using namespace cimg_library;
 
 
 GreyScale::GreyScale() {
-    //std::filesystem::path dir ("..");
-    //this->filePath = dir / "data";
-    this->filePath = "../data";
+    this->filePath = ".." + fileSeparator + "data";
     this->fileName = "dino.bmp";
-    //this->fileName = "white.bmp";
+    this->dManager = DataManager();
 }
-
-
-GreyScale::GreyScale(std::string filePath, std::string fileName) {
-    this->filePath = filePath;
-    this->fileName = fileName;
-}
-
 
 void GreyScale::loadFile() {
-    //std::string fullFilePath = this->filePath / this->fileName;
-    std::string fullFilePath = this->filePath + "/" + this->fileName;
+    this->loadFile(this->filePath, this->fileName);
+}
+
+void GreyScale::loadFile(std::string filePath, std::string fileName) {
+    this->filePath = filePath;
+    this->fileName = fileName;
+    std::string fullFilePath = this->filePath + fileSeparator + this->fileName;
     const char* filename_input = fullFilePath.c_str();
     this->img = CImg<u_char>(filename_input);
 }
 
 
 void GreyScale::makeGreyScaleSerial() {
-    int width = this->img.width(),
-        height = this->img.height(),
-        depth = this->img.depth();
+    int width   = this->img.width(),
+        height  = this->img.height(),
+        depth   = this->img.depth();
 
-    /* pointer to image pixels. Colors are rranged in contiguous memory.
+    /* pointer to image pixels. Colors are arranged in contiguous memory.
     Eg: {RRR...RRRGGG...GGGBBB...BBB} */
     u_char* arr = this->img.data();
     size_t  length = width * height * depth,
@@ -45,10 +39,11 @@ void GreyScale::makeGreyScaleSerial() {
     // pointers to the start of each of the color channels
     u_char  * R = &arr[0 * rgbOffset],
             * G = &arr[1 * rgbOffset],
-            * B = &arr[2 * rgbOffset];
+            * B = &arr[2 * rgbOffset],
+            greyScaleValue;
 
     for (size_t i = 0; i < length; ++i) {
-        u_char greyScaleValue = R[i] * 0.3 + G[i] * 0.59 + B[i] * 0.11;
+        greyScaleValue = R[i] * 0.3 + G[i] * 0.59 + B[i] * 0.11;
         R[i] = greyScaleValue;
         G[i] = greyScaleValue;
         B[i] = greyScaleValue;
@@ -56,15 +51,17 @@ void GreyScale::makeGreyScaleSerial() {
 }
 
 
-void GreyScale::makeGreyScaleParallel(int numBlocks, int numGrids) {
+void GreyScale::makeGreyScaleParallel(int numBlocks) {
     /* pointer to image pixels. Colors are rranged in contiguous memory.
     Eg: {RRR...RRRGGG...GGGBBB...BBB} */
     u_char  * hostImageArr = this->img.data(),
             * devImageArr;
-    int channels = 3,
-        * devImageDims,
+    int * devImageDims,
+        channels = 3,
         length = this->img.width() * this->img.height() * this->img.depth() * channels,
         imageDims[2] = { this->img.width(), this->img.height() };
+
+    dManager.startTimer(); // start of CUDA procedures
 
     cudaMalloc((void**)&devImageArr, length * sizeof(u_char));
     cudaMalloc((void**)&devImageDims, 2 * sizeof(unsigned long));
@@ -78,20 +75,22 @@ void GreyScale::makeGreyScaleParallel(int numBlocks, int numGrids) {
     cudaMemcpy(hostImageArr, devImageArr, length * sizeof(u_char), cudaMemcpyDeviceToHost);
     cudaFree(devImageArr);
     cudaFree(devImageDims);
+
+    dManager.stopTimer(); // end of CUDA procedures
 }
 
 
 __global__ void mykernel(u_char* imageArr, int* devImageDims) {
-    int width = devImageDims[0],
-        height = devImageDims[1],
-        length = width * height,
-        x = threadIdx.x + blockIdx.x * blockDim.x,
-        y = threadIdx.y + blockIdx.y * blockDim.y,
-        offset = x + y * blockDim.x * gridDim.x;
+    int width   = devImageDims[0],
+        height  = devImageDims[1],
+        length  = width * height,
+        x       = threadIdx.x + blockIdx.x * blockDim.x,
+        y       = threadIdx.y + blockIdx.y * blockDim.y,
+        offset  = x + y * blockDim.x * gridDim.x;
 
     // a single channel takes up "length" number of ints
     if (offset > length*3) return;
-
+    // pointers to the start of each of the color channels
     u_char  * R = &imageArr[0 * length],
             * G = &imageArr[1 * length],
             * B = &imageArr[2 * length],
@@ -111,8 +110,11 @@ void GreyScale::display() {
 
 void GreyScale::writeFile() {
     std::string outputFileName = "GreyScale_" + fileName;
-    //std::filesystem::path outFilePath = this->filePath / outputFileName;
-    std::string outFilePath = this->filePath + "/" + outputFileName;
+    std::string outFilePath = this->filePath + fileSeparator + outputFileName;
     const char* filename_output = outFilePath.c_str();
     this->img.save(filename_output);
+}
+
+double GreyScale::getTimeElapsed(){
+    return dManager.getTimeElapsed();
 }
--- src/GreyScale.cuh
+++ src/GreyScale.cuh
@@ -1,10 +1,15 @@
-#ifndef GREYSCALE_HPP
-#define GREYSCALE_HPP
+#ifndef GREYSCALE_CUH
+#define GREYSCALE_CUH
 
 #include <string>
-
-
 #include "CImg.h"
+#include "DataManager.cuh"
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+static const std::string fileSeparator="\\";
+#else
+static const std::string fileSeparator="/";
+#endif
 
 class GreyScale{
     private:
@@ -13,18 +18,20 @@ class GreyScale{
         std::string fileName;
         size_t gridSize;
         size_t blockSize;
+        DataManager dManager;
 
     public:
         GreyScale();
-        GreyScale(std::string filePath, std::string fileName);
         void loadFile();
+        void loadFile(std::string filePath, std::string fileName);
         void makeGreyScaleSerial();
-        void makeGreyScaleParallel(int numBlocks, int numGrids);
+        void makeGreyScaleParallel(int numBlocks);
         void display();
         void writeFile();
+        double getTimeElapsed();
 };
 
 // kernel functions must be global
 __global__ void mykernel(u_char* imageArr, int* devImageDims);
 
-#endif // GREYSCALE_HPP
+#endif // GREYSCALE_CUH
